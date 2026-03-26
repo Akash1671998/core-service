3e8207903af83f96de2d522294c06372abf82450
Akash1671998 <akash.vishwakarma@coraltele.com>
2026-03-26 11:46:51 +0530

    changes for read jwt token secret key and expiration time  from configuration file

--- src/main/java/core/service/security/JwtService.java
+++ src/main/java/core/service/security/JwtService.java
@@ -2,6 +2,8 @@ package core.service.security;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
@@ -9,8 +11,11 @@ import java.util.Date;
 
 @Service
 public class JwtService {
+    @Value("${jwt.secret}")
+    private String SECRET;
 
-    private final String SECRET = "mysecretkeymysecretkeymysecretkey";
+    @Value("${jwt.expiration}")
+    private long expiration;
 
     private Key getSignKey() {
         return Keys.hmacShaKeyFor(SECRET.getBytes());
@@ -20,7 +25,7 @@ public class JwtService {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
